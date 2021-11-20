f67d52f1d62a4d4d00261729e076df087ef886ba
Panov Dmitriy <panov92@gmail.com>
2021-11-20 19:10:44 +0300

    mine transactions

--- client/src/pages/TransactionPool.js
+++ client/src/pages/TransactionPool.js
@@ -1,16 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import { Card } from 'react-bootstrap';
+import { Card, Button } from 'react-bootstrap';
 import Transaction from '../components/Transaction';
+import { toast } from 'react-toastify';
 import axios from 'axios';
+import { withRouter } from 'react-router-dom';
 
-const TransactionPool = () => {
+const TransactionPool = props => {
+  const { history } = props;
+
   const [transactionPoolMap, setTransactionPoolMap] = useState({});
 
   const fetchTransactionPoolMap = async () => {
-
     const res = await axios.get('http://localhost:3005/api/transaction-pool-map');
-
     setTransactionPoolMap(res.data);
+  }
+
+  const fetchMineTransactions = async () => {
+    const res = await axios.get('http://localhost:3005/api/mine-transactions');
+
+    if (res.status === 200) {
+      toast.success("Success");
+      history.push('/blocks');
+    } else {
+      toast.error("The mine-transactions block request did not complete");
+    }
   }
 
   useEffect(() => {
@@ -22,22 +35,32 @@ const TransactionPool = () => {
       <Card className="bg-dark-30 text-white">
         <Card.Header>Transaction Pool</Card.Header>
         <Card.Body>
-          {
-            Object.values(transactionPoolMap).map(transaction => {
+          {Object.keys(transactionPoolMap).length ? (
+            (() => {
               return (
-                <div key={transaction.id}>
-                  <Transaction transaction={transaction} />
-                  <hr />
-                </div>
+                <>
+                  {Object.values(transactionPoolMap).map(transaction => {
+                    return (
+                      <div key={transaction.id}>
+                        <Transaction transaction={transaction} />
+                        <hr />
+                      </div>
+                    )
+                  })}
+                  <div className="d-flex justify-content-center">
+                    <Button onClick={fetchMineTransactions}>
+                      Mine blocks
+                    </Button>
+                  </div>
+                </>
               )
-            })
-          }
-          {!Object.keys(transactionPoolMap).length && 'No transactions'}
+            })()
+          ) : 'No transactions'}
         </Card.Body>
       </Card>
     </div>
   )
 }
 
-export default TransactionPool;
+export default withRouter(TransactionPool);
 
